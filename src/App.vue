a5ad32a33a69b24ca29c387b8aca20a2dc62a180
Minish144 <varlamow.col@yahoo.com>
2020-07-30 21:55:08 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Main/>
+    <Viewer/>
+  </div>
+</template>
+
+<script>
+import Main from '@/components/Menu'
+import Viewer from '@/components/Viewer'
+
+export default {
+  name: 'App',
+  components: {
+    Main, Viewer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Viewer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <video src="https://animethemes.moe/video/Clannad-ED1.webm" autoplay="autoplay"></video>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    video {
+        width: 50%;
+        height: auto;
+    }
+</style>
